b968917f14a94d191414144a6bf7dd4969a1e0b7
Thiago Vasconcelos <vasconcelosthiago@outlook.com>
2022-02-08 16:50:40 -0300

    Segundo desafio feito

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-document.querySelector('.tecla_pom');
+function somPom(){
+
+    
+   
+    document.querySelector('#som_tecla_pom').play();
+
+
+}
+
+document.querySelector('.tecla_pom').onclick = somPom;
+
+
+
+function tocaSomClap(){
+
+    
+  
+    document.querySelector('#som_tecla_clap').play();
+
+
+}
+
+document.querySelector('.tecla_clap').onclick = tocaSomClap;
